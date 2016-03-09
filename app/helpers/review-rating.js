0abf6fca4f36a0bd99bea9a59f1287874072ebbf
Midori Bowen <midori.p.bowen@gmail.com>
2016-03-08 17:24:24 -0800

    Fix star-rating display bug.

--- app/helpers/review-rating.js
+++ app/helpers/review-rating.js
@@ -2,21 +2,19 @@ import Ember from 'ember';
 
 export function reviewRating(params) {
 
-  // CURRENT BUG: Stars are not displaying properly - all ratings are displaying the first if statement (1 star)
-
-  var bookRating = params[0].get('rating');
+  var bookRating = parseInt(params[0]);
   var filledStar = "&#9733;";
   var emptyStar = "&#9734;";
 
-  if (bookRating = 1) {
+  if (bookRating === 1) {
     return Ember.String.htmlSafe(filledStar + emptyStar + emptyStar + emptyStar + emptyStar);
-  } else if (bookRating = 2) {
+  } else if (bookRating === 2) {
     return Ember.String.htmlSafe(filledStar + filledStar + emptyStar + emptyStar + emptyStar);
-  } else if (bookRating = 3) {
+  } else if (bookRating === 3) {
     return Ember.String.htmlSafe(filledStar + filledStar + filledStar + emptyStar + emptyStar);
-  } else if (bookRating = 4) {
+  } else if (bookRating === 4) {
     return Ember.String.htmlSafe(filledStar + filledStar + filledStar + filledStar + emptyStar);
-  } else if (bookRating = 5) {
+  } else if (bookRating === 5) {
     return Ember.String.htmlSafe(filledStar + filledStar + filledStar + filledStar + filledStar);
   }
 
